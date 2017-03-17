774abbdf1ccdc02f45e8e44aaa07be69d701a840
Niels Abildgaard <niels.abildgaard@gmail.com>
2017-03-17 12:32:06 +0100

    My work so far

--- week8/src/test/scala/fpinscala/laziness/StreamSpecNroeMaumNiec.scala
+++ week8/src/test/scala/fpinscala/laziness/StreamSpecNroeMaumNiec.scala
@@ -19,9 +19,17 @@ import stream00._    // uncomment to test the book solution
 // import stream01._ // uncomment to test the broken headOption implementation
 // import stream02._ // uncomment to test another version that breaks headOption
 
+// here's our test exception that goes boom.
+case class Bomb() extends java.lang.Exception("Boom!")
+
+// pirate my pad http://piratepad.net/9t4ATxObgt
+
 class StreamSpecNroeMaumNiec extends FlatSpec with Checkers {
 
   import Stream._
+
+  //niels c really wanted to build an infinite unit stream that throws bombs, so here goes:
+  def infiniteBombStream: Stream[Unit] = cons({throw new Bomb}, infiniteBombStream)
 
   behavior of "headOption"
 
@@ -36,7 +44,7 @@ class StreamSpecNroeMaumNiec extends FlatSpec with Checkers {
 
   // In ScalaTest we use the check method to switch to ScalaCheck's internal DSL
   def genNonEmptyStream[A] (implicit arbA :Arbitrary[A]) :Gen[Stream[A]] =
-    for { la <- arbitrary[List[A]] suchThat (_.nonEmpty)}
+    for { la <- arbitrary[List[A]] suchThat (_.nonEmpty) }
     yield list2stream (la)
 
   // a property test:
@@ -44,11 +52,31 @@ class StreamSpecNroeMaumNiec extends FlatSpec with Checkers {
   it should "return the head of the stream packaged in Some (02)" in check {
     // the implict makes the generator available in the context
     implicit def arbIntStream = Arbitrary[Stream[Int]] (genNonEmptyStream[Int])
+    
     ("singleton" |:
       Prop.forAll { (n :Int) => cons (n,empty).headOption == Some (n) } ) &&
     ("random" |:
       Prop.forAll { (s :Stream[Int]) => s.headOption != None } )
+  }
 
+  it should "not force the tail of the stream (03)" in {
+    val explodingStream = cons(1 + 0, cons({throw new Bomb; 3}, empty))
+    assert(explodingStream.headOption == Some(1))
+  }
+  
+  behavior of "take"
+
+  it should "should not force any heads nor any tails (04)" in check {
+    Prop.forAll { (n: Int) => { infiniteBombStream.take(n); empty == empty } }
+  } 
+
+  it should "not ever force the (n+1)st head ever" in check {
+    def nNumbersThenBombs(n: Int): Stream[Int] = {
+      if(n == 0) infiniteBombStream.map(x => 1)
+      else cons(n, nNumbersThenBombs(n - 1))
+    }
+    
+    Prop.forAll { (n: Int) => n < 0 || nNumbersThenBombs(n).take(n).toList.length == n }
   }
 
 }
