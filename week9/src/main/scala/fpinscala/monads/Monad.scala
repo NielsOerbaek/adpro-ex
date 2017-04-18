4632dd2c0bedb8fa3c79c759b837a05be17812e4
Niels Abildgaard <niels.abildgaard@gmail.com>
2017-04-18 15:28:03 +0200

    indent dat shieeet yo

--- week9/src/main/scala/fpinscala/monads/Monad.scala
+++ week9/src/main/scala/fpinscala/monads/Monad.scala
@@ -68,7 +68,7 @@ trait Monad[F[_]] {
 
   // Exercise 16 (CB11.7)
   def compose[A,B,C] (f: A => F[B], g: B => F[C]): A => F[C] =
-  a => flatMap (f(a)) (g)
+    a => flatMap (f(a)) (g)
 
   // Just for fun: Implementing flatMap using compose.
   def flatMap1[A,B] (ma: F[A]) (f: A => F[B]) :F[B] =   
