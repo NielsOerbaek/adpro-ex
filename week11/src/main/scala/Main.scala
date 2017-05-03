44a61348b3241d4bd569801e6483468996ed1d65
niels <niec@itu.dk>
2017-05-03 10:59:37 +0200

    Added miniproject 2 files

---
+++ week11/src/main/scala/Main.scala
@@ -0,0 +1,78 @@
+// Advanced Programming. Andrzej Wasowski. IT University
+// To execute this example, run "sbt run" or "sbt test" in the root dir of the project
+// Spark needs not to be installed (sbt takes care of it)
+
+import org.apache.spark.ml.feature.Tokenizer
+import org.apache.spark.sql.Dataset
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types._
+
+
+object Main {
+
+	type Embedding       = (String, List[Double])
+	type ParsedReview    = (Integer, String, Double)
+
+	org.apache.log4j.Logger getLogger "org"  setLevel (org.apache.log4j.Level.WARN)
+	org.apache.log4j.Logger getLogger "akka" setLevel (org.apache.log4j.Level.WARN)
+	val spark =  SparkSession.builder
+		.appName ("Sentiment")
+		.master  ("local[9]")
+		.getOrCreate
+
+  import spark.implicits._
+
+	val reviewSchema = StructType(Array(
+			StructField ("reviewText", StringType, nullable=false),
+			StructField ("overall",    DoubleType, nullable=false),
+			StructField ("summary",    StringType, nullable=false)))
+
+	// Read file and merge the text abd summary into a single text column
+
+	def loadReviews (path: String): Dataset[ParsedReview] =
+		spark
+			.read
+			.schema (reviewSchema)
+			.json (path)
+			.rdd
+			.zipWithUniqueId
+			.map[(Integer,String,Double)] { case (row,id) => (id.toInt, s"${row getString 2} ${row getString 0}", row getDouble 1) }
+			.toDS
+			.withColumnRenamed ("_1", "id" )
+			.withColumnRenamed ("_2", "text")
+			.withColumnRenamed ("_3", "overall")
+			.as[ParsedReview]
+
+  // Load the GLoVe embeddings file
+
+  def loadGlove (path: String): Dataset[Embedding] =
+		spark
+			.read
+			.text (path)
+      .map  { _ getString 0 split " " }
+      .map  (r => (r.head, r.tail.toList.map (_.toDouble))) // yuck!
+			.withColumnRenamed ("_1", "word" )
+			.withColumnRenamed ("_2", "vec")
+			.as[Embedding]
+
+  def main(args: Array[String]) = {
+
+    val glove  = loadGlove ("C:\\adpro-bigthings\\glove.6B.50d.txt") // FIXME
+    val reviews = loadReviews ("C:\\adpro-bigthings\\Pet_Supplies_5.json") // FIXME
+
+    // replace the following with the project code
+
+    glove.show
+    reviews.show
+
+
+    val tokenizer = new Tokenizer().setInputCol("text").setOutputCol("words")
+	val tokenized = tokenizer.transform(reviews)
+	tokenized.select("words").take(3).foreach(println)
+
+	tokenized.show
+
+		spark.stop
+  }
+
+}
