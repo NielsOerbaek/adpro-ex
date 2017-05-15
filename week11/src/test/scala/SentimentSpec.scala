f7f826f7d92da4f22380b985b77761d430ecdcca
niels <niec@itu.dk>
2017-05-15 09:53:16 +0200

    Niels made a test but failed to push it and stuff

--- week11/src/test/scala/SentimentSpec.scala
+++ week11/src/test/scala/SentimentSpec.scala
@@ -1,4 +1,4 @@
-import org.scalatest.{FreeSpec, Matchers, BeforeAndAfterAll}
+import org.scalatest.{BeforeAndAfterAll, FreeSpec, Matchers}
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.Dataset
 
@@ -16,28 +16,42 @@ class SentimentSpec extends FreeSpec with Matchers with BeforeAndAfterAll {
 
   import spark.implicits._
 
-  val glove = Main.loadGlove("path/to/glove/file/in/your/filesystem")
-  val reviews = Main.loadReviews("path/to/reviews/file/in/your/filesystem")
+  val glove = Main.loadGlove("C:\\adpro-bigthings\\glove.6B.300d.txt")
+  val reviews = Main.loadReviews("C:\\adpro-bigthings\\Amazon_Instant_Video_5.json")
 
   "something to do with sentiments in texts or some such" - {
     "tokenization" - {
       "correctly splits a short sentence into meaningful tokens" in {
-        ??? // Main.getTokens()
+        val ds = Seq((1,"This Sentence Has Five Words", 0.0))
+          .toDS
+          .withColumnRenamed ("_1", "id" )
+          .withColumnRenamed ("_2", "text")
+          .withColumnRenamed ("_3", "overall")
+          .as[Main.ParsedReview]
+
+        val tokens = Main.getTokens(ds)
+          .select("token")
+          .rdd
+          .map(r => r(0))
+          .collect()
+          .toList
+
+        assert(tokens == List("this", "sentence", "has", "five", "words"))
       }
     }
 
     "transformRating" - {
       "transforms a low rating to 0" in {
-        Main.transformRating(1.2).should.equal(0)
+        assert(Main.transformRating(1.2) == 0)
       }
 
       "transforms a mediocre rating to 1" in {
-        Main.transformRating(3.0).should.equal(1)
+        assert(Main.transformRating(3.0) == 1)
       }
 
       "transforms a good rating to 2" in {
-        Main.transformRating(3.1).should.equal(2)
-        Main.transformRating(4.0).should.equal(2)
+        assert(Main.transformRating(3.1) == 2)
+        assert(Main.transformRating(4.0) == 2)
       }
     }
     
@@ -54,7 +68,7 @@ class SentimentSpec extends FreeSpec with Matchers with BeforeAndAfterAll {
     }
 
     "n-fold cross-validation" - {
-      "probably runs a perceptron..." taggedAs Ignore in {
+      "probably runs a perceptron..." ignore {
         // this is quite hard to test :-( we have done manual tests of the output
       }
     }
